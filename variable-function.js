f1d1080bc3a1e51e7b8ed9249a3fc10e9e39ce3f
songha-5 <shn0518@gmail.com>
2026-01-09 20:37:48 +0900

    feat: 빛의 속도 문구 추가

--- variable-function.js
+++ variable-function.js
@@ -7,7 +7,7 @@ let movieTitle = '3 IDIOTS'
 console.log(movieTitle)
 
 // 2. "빛의 속도(299,792,458 m/s)"를 담을 상수를 선언하세요.
-const LIGHT_SPEED = "299,792,458 m/s"
+const LIGHT_SPEED = "빛의 속도(299,792,458 m/s)"
 console.log(LIGHT_SPEED)
 
 // 3. "이메일 인증 완료 여부"를 확인하는 불리언 타입 변수를 선언하고 적절한 값을 할당하세요.
