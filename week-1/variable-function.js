92596de8885ea3d7b628bf40812759cccf01bcad
songha-5 <shn0518@gmail.com>
2026-01-09 19:58:13 +0900

    feat: 이메일 인증 완료 여부 상수/불리언타입 추가

--- week-1/variable-function.js
+++ week-1/variable-function.js
@@ -11,6 +11,8 @@ const LIGHT_SPEED = "299,792,458 m/s"
 console.log(LIGHT_SPEED)
 
 // 3. "이메일 인증 완료 여부"를 확인하는 불리언 타입 변수를 선언하고 적절한 값을 할당하세요.
+const EMAIL_VERIFICATION_BOOLEAN = Boolean(true)
+console.log(EMAIL_VERIFICATION_BOOLEAN)
 
 // 4. "상품 재고 수량(예: 47개)"을 담는 변수를 선언하고 숫자 값을 할당하세요.
 // 5. "회원 포인트(예: 15,800점)"를 담는 변수를 작성하세요.
