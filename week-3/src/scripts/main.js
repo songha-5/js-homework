3cc0e30657299257668c3b1b5e4dc7a25d15294d
songha-5 <shn0518@gmail.com>
2026-01-25 12:25:34 +0900

    feat: tab버튼과 content의 active 추가

--- week-3/src/scripts/main.js
+++ week-3/src/scripts/main.js
@@ -8,35 +8,23 @@ for (let i = 0, l = tabButton.length; i < l; i++ ) {
   console.log(i)
   // 각각의 탭 버튼을 눌렀을때 클릭 이벤트
   tabButton[i].addEventListener('click', () => {
-    tabButton.classList.remove('active')
+    // tabButton.classList.remove('active')
     tabButton[i].classList.add('active')
-    tabButton.setAttribute('aria-selected', 'flase')
+    // tabButton.setAttribute('aria-selected', 'flase')
     tabButton[i].setAttribute('aria-selected', 'true')
+
+
+    tabContent[i].classList.add('tab')
   })
 }
 
 
-
 // ==================
 
-// data-action="account-inquiry" 
-// 이면 data-tab="account-inquiry"에 add tab
-// if
-// tab_button이 가지고있는 data-action === data-tab값과 동일한거에
-// data-tab="account_inquiry" add tab하기
-// (dataset.dataAction === dataset.dataTab ) {
-//   해당 dataset.dataTab
-// }
-
-// data-action="account-inquiry"값을 가져
 
 
 
 
-//tab_button에 클래스 추가 active
-// 기존 aria-selected="true"를 false로 변경
-// 선택된 aria-selected="true"로 변경 
-// 선택된 data-tab에 tab class추가
 
 
 
