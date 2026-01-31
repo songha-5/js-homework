9f62ff6ab7b2d9ed02a6b7e87d9d780ae7984d17
songha-5 <shn0518@gmail.com>
2026-01-31 22:35:13 +0900

    feat: JS 큰 틀  작성

---
+++ week-4/src/scripts/main.js
@@ -0,0 +1,19 @@
+// TODO
+// querySelectorAll로 `data-select="card"`를 선택
+// - closest으로 `data-card="card"`선택해서 이벤트 추가
+// - target으로 맞는지 확인 후 반환/ null이면 바로 리턴
+// - 맞으면 add('.checked')
+// - 다른것들은 제거 ( 어떻게 할지 고민 ) // 밖에서 선언한거 불러오면 오류생김
+// - 변수체크한거에서 closest(.card-section).querySelect('.card').remove 가능한지
+
+// forEach???
+// 데이터를 배열로 넣을때? 그런데 처음 데이터를 뿌릴때 넣을거라 상관없지 않을까?
+
+// addEventListener > 안에 함수로 넣기
+// card_noti에 checked추가 
+// data-plan값을 가져와서 .card-noti의 `data-plan`에 textContent로 넣기
+// data-price값을 가져와서 .card-noti의 `data-price`에 textContent로 넣기
+
+// 할수있으면 하기
+// .card_noti는 누르고 3초뒤에 checked 사라지기
+
